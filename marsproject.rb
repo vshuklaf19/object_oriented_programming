cee787149f62e2db681aed81f88526309d0d15f6
Vivek Shukla <vivek.f19@gmail.com>
2015-01-22 17:03:28 -0500

    Made it more functional. Prints the right values.

--- marsproject.rb
+++ marsproject.rb
@@ -3,11 +3,20 @@ class Rover
 		@x = x
 		@y = y
 		@direction = direction
+    puts self
 	end
 
-	def read_instruction(instructions)
-	#"hello".split("")=> ["h", "e", "l", "l", "o"]
-		each_direction = instructions.split("")
+	def process_instructions(instructions)
+    instructions.each_char do |c|
+      if c == "M"
+        move
+      else 
+        turn(c)
+      end
+          
+      puts self    
+    end
+    
 	end
 
 	def move
@@ -25,9 +34,6 @@ class Rover
 	end
 
   def turn(instruction)
-#turn affects the direction of the rover. Depending on the current direction 
-#of the rover, the new direction will be determined with either turning L or R.
-    raise "Too many characters" if instruction.length > 1
     if @direction == "N" && instruction="L" || @direction == "S" && instruction="R"
       @direction = "W"
     elsif @direction == "W" && instruction="L" || @direction == "E" && instruction="R"
@@ -39,38 +45,23 @@ class Rover
     end
   end
 
-  def current_location
+  def to_s
     "Current location of Rover: #{@x} #{@y} #{@direction}"
   end
-
-  def current_direction
-    "Current direction of Rover: #{@direction}"
-  end
 end
+print "Enter starting position: "
+x, y, d = gets.chomp.split
 
-# INPUT AND OUTPUT
-# Test Input:
-# 5 5                  > Our first line is our plateau size. 
-# 1 2 N                > Lines 2 and 3 are paired, with the first being our initial start position and header,
-# LMLMLMLMM            > and the second line being a series of move and turn instructions.
-# 3 3 E                > Another 2-line pair, 1 pair per rover.
-# MMRMMRMRRM
 
-# Expected Output:
-# 1 3 N                > When rover bot is done doing its instructions, output its current position
-# 5 1 E                > The first line is the first rover, the second line is the next rover
-# puts "Test Input: "
-# plateau_size = gets.chomp
-# initial_start_position = gets.chomp
-# move_and_turn_instructions = gets.chomp
+print "Enter Instructions: "
+instructions = gets.chomp
 
-mars_rover = Rover.new(1,2,"N")
-puts mars_rover.read_instruction("LMRLMRLMR")
+mars_rover = Rover.new(x.to_i, y.to_i, d)
+mars_rover.process_instructions(instructions)
 
-mars_rover.move
-mars_rover.move
-mars_rover.turn("L")
-mars_rover.turn("L")
-mars_rover.turn("L")
-puts mars_rover.current_location
-puts mars_rover.current_direction
+# mars_rover.move
+# mars_rover.move
+# mars_rover.turn("L")
+# mars_rover.turn("L")
+# mars_rover.turn("L")
+# puts mars_rover
